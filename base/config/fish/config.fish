6ec89e377c6fabd857a2dc890de7fa27f2650153
redtankd <redtankd@qq.com>
2018-05-06 18:29:40 +0800

    update environment variables

--- base/config/fish/config.fish
+++ base/config/fish/config.fish
@@ -3,7 +3,9 @@
 if status --is-interactive
     alias cdd='cd ~/dev'
     alias cdg='cd ~/dev/git/'
-
+    alias cdgc='cd ~/dev/git/cfets'
+    alias cdgg='cd ~/dev/git/github'
+    
     alias psg='ps -ef|grep'
     alias g='git'
 end
--- rust/config/fish/config.fish
+++ rust/config/fish/config.fish
@@ -1,10 +1,10 @@
-
 # for Rust
 
-if status --is-interactive
-    alias cdgc='cd ~/dev/git/cfets'
-    alias cdgg='cd ~/dev/git/github'
+set -x RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
 
+set PATH $PATH ~/.cargo/bin
+
+if status --is-interactive
     alias c='cargo'
     alias r='rustup'
 end
