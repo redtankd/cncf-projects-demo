8221b295ad8500bedf67481775f3ada1aef1e039
redtankd <redtankd@qq.com>
2020-01-13 20:18:18 +0800

    remove kcov

--- docker/rust/config/fish/config.fish
+++ docker/rust/config/fish/config.fish
@@ -1,6 +1,7 @@
 # for Rust
 
 set -x RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
+set -x RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
 
 set PATH $PATH ~/.cargo/bin
 
